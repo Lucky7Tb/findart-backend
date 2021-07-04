c7e679540ca6d0e9484f145f730d7e7ae55981cb
Lucky7Tb <Luckytri101@gmail.com>
2021-07-04 13:47:06 +0700

    change file upload

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -3,29 +3,27 @@
 namespace App\Helper;
 
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Storage;
-
 class Helper
 {
 
 	public static function saveImage($image, $savePath)
 	{
-		$newFileName = date('YmdHis').'-'.$image->getClientOriginalName();
+		$newFileName = date('YmdHis') . '-' . $image->getClientOriginalName();
 		$newFileName = Str::replace(' ', '', $newFileName);
 
-		Storage::putFileAs($savePath, $image, $newFileName);
+		$image->move(public_path('disk'), $newFileName);
 
-		$fileUrl = asset('storage/images/'.$newFileName);
+		$fileUrl = asset('disk/' . $newFileName);
 
 		return $fileUrl;
 	}
 
 	public static function deleteImage($urlImage)
 	{
-		$imagePath = Str::remove(env('APP_URL').'/public/storage', $urlImage);
+		$imagePath = Str::remove(env('APP_URL'), $urlImage);
 
-		if (Storage::disk('public')->exists($imagePath)) {
-			Storage::disk('public')->delete($imagePath);
+		if (file_exists(public_path() . $imagePath)) {
+			unlink(public_path() . $imagePath);
 		}
 	}
 }
